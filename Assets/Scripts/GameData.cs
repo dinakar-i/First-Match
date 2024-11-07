146397eea87af0a05a4fafe1fd79a870f1271210
dinakar-i <115007967+dinakar-i@users.noreply.github.com>
2024-11-07 12:29:29 +0530

    L Push

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public static class GameData 
+
+public static class GameData
 {
-    public static int rows=12,cols=12;
+    public static int rows = 12, cols = 12;
 }
